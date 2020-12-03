6820f5e4bdf69850ec45b29c081053d046acc6c3
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2020-12-03 17:17:01 +0200

    3/12/2020

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var table = $('#example').DataTable({
+    dom: "B<'row'<'col-sm-6 col-md-6'f><'ml-3'l>>rtip",
+    "paging":   false,
+    "ordering": false,
+    "info":     false,
+    "scrollX": true,
+        buttons: [
+
+    {
+        extend: "print",
+        exportOptions: {
+            columns: ":visible",
+            autoPrint: true,
+            orientation: "landscape",
+        },
+},
+    {
+        extend: "csv",
+        exportOptions: {
+            columns: ":visible",
+            autoPrint: true,
+            orientation: "landscape",
+    },
+    },
+    {
+        extend: "pdf",
+        exportOptions: {
+            columns: ":visible",
+            autoPrint: true,
+            orientation: "landscape",
+    },
+    },
+    {
+        extend: "excel",
+        exportOptions: {
+            columns: ":visible",
+            autoPrint: true,
+            orientation: "landscape",
+        },
+    },
+    {
+        extend: "copy",
+        exportOptions: {
+            columns: ":visible",
+            autoPrint: true,
+            orientation: "landscape",
+        },
+    },
+    "colvis",
+],
+    "language": {
+    search: '<strong style="display:none">البحث</strong>',
+    searchPlaceholder: 'بحث',
+    },
+    columnDefs: [{
+        targets: [-1],
+        className: "hide_column"
+    }]
+    });
+    $('.delete-button').click(function() {
+      selectedRow = $(this).closest('tr');
+      idSelectedRow = $(this).closest('tr').find(".hide_column").text();
+  
+});
+
+/*$(document).ready(function() {
+    $('table.display').DataTable({
+            "paging":   false,
+    "ordering": false,
+    "info":     false,
+    "scrollX": true
+    });
+} );*/
